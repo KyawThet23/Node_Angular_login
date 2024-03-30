73fb624b55e75395adbd50d611d2bfcdfda133db
kyawThetHtun <kyawthe235@gmail.com>
2024-03-30 20:25:44 +0630

    sign_up and auth services

---
+++ backend/app/config/auth.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  secret: "KTH-secret-key"
+};
--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,11 @@ app.use(
   })
 );
 
+const db = require("./app/models");
+const Role = db.role;
+
+db.sequelize.sync();
+
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to KT's application." });
 });
@@ -26,3 +31,20 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 }); 
+
+function initial() {
+  Role.create({
+    id: 1,
+    name: "user",
+  });
+
+  Role.create({
+    id: 2,
+    name: "moderator",
+  });
+
+  Role.create({
+    id: 3,
+    name: "admin",
+  });
+}
